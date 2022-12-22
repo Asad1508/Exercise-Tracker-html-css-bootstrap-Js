7b8b3f13abad0226ee2908e1e3428aa46a9503cd
Asadbutt <abutt1508@gmail.com>
2022-12-22 16:08:52 +0500

    done template all 100%

---
+++ APIfakedata.js
@@ -0,0 +1,46 @@
+
+
+const data=[
+    {
+       name:"Push ups",
+       description:"These are push ups",
+       type:"push ups",
+       duration:"5 mins",
+       date:"12/2/2022"
+    },
+   
+    {
+       name:"Running",
+       description:"i am running",
+       type:"Run",
+       duration:"5 mins",
+       date:"12/2/2022"
+    },
+   
+    {
+       name:"Push ups",
+       description:"These are push ups",
+       type:"push ups",
+       duration:"5 mins",
+       date:"12/2/2022"
+    },
+   
+    {
+       name:"Hand set",
+       description:"These are hand sets",
+       type:"hand sets",
+       duration:"5 mins",
+       date:"12/2/2022"
+    },
+   
+    {
+       name:"Push ups",
+       description:"These are push ups",
+       type:"push ups",
+       duration:"5 mins",
+       date:"12/2/2022"
+    },
+   
+    
+]
+export default data;
